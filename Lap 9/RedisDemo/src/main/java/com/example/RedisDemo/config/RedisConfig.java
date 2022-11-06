49bb54653d6ccf5638eb5b975011d727ea75f13e
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-11-06 18:51:19 +0700

    update redis curd  opshash,value

--- Lap 9/RedisDemo/src/main/java/com/example/RedisDemo/config/RedisConfig.java
+++ Lap 9/RedisDemo/src/main/java/com/example/RedisDemo/config/RedisConfig.java
@@ -1,42 +1,27 @@
 package com.example.RedisDemo.config;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
+import com.example.RedisDemo.entity.User;
 @Configuration
 public class RedisConfig {
-    @Value("${redis.host}")
-    private String redisHost;
-
-    @Value("${redis.port}")
-    private int redisPort;
-
-
+ 
+    // Setting up the Jedis connection factory.
+ 
+      
+    // Setting up the Redis template object.
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        // Tạo Standalone Connection tới Redis
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
-    }
-
-    @Bean
-    @Primary
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        // tạo ra một RedisTemplate
-        // Với Key là Object
-        // Value là Object
-        // RedisTemplate giúp chúng ta thao tác với Redis
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
+    public RedisTemplate<String, User> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, User> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // Add some specific configuration here. Key serializers, etc.
         return template;
     }
 }
-
 
--- Lap 9/RedisDemo/src/main/java/com/example/RedisDemo/controller/RedisController.java
+++ Lap 9/RedisDemo/src/main/java/com/example/RedisDemo/controller/RedisController.java
@@ -1,23 +1,71 @@
 package com.example.RedisDemo.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.RedisDemo.entity.User;
+import com.google.gson.Gson;
 
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
+	static Gson gson = new Gson();
 	@Autowired
-	private RedisTemplate redisTemplate;
-	
-	@PostMapping("/user")
+	private RedisTemplate<String, User> redisTemplate;
+	@PostMapping("/userhash")
 	public User saveUser(@RequestBody User user) {
-		 redisTemplate.opsForValue().set(user.getId(),user);
+		
+		//redisTemplate.opsForValue().set(user.getId(), user);
+		redisTemplate.opsForHash().put("User", user.getId(), user);
+		System.out.println("user"+user);
 		 return user;
 	}
+	
+	@PostMapping("/uservalue")
+	public User saveUserValue(@RequestBody User user) {
+		
+		redisTemplate.opsForValue().set(user.getId(), user);
+//		redisTemplate.opsForHash().put("User", user.getId(), user);
+		System.out.println("user"+user);
+		 return user;
+	}
+	@GetMapping("/uservalue/{id}")
+	public User getUserValue(@PathVariable("id") String id) {
+		
+//		
+		 return redisTemplate.opsForValue().get(id);
+	}
+	@GetMapping("/userhash/{id}")
+	public Object findById(@PathVariable("id") String id) {
+	    return  redisTemplate.opsForHash().get("User", id);
+	}
+	@GetMapping("/listuser")
+	public Map<Object, Object> getlist() {
+	    return  redisTemplate.opsForHash().entries("User");
+	}
+	
+	@DeleteMapping("/uservalue/{id}")
+	public Boolean deleteUser( @PathVariable("id") String id) {
+	    return  redisTemplate.delete(id);
+	}
+	@DeleteMapping("/userhash/{id}")
+	public String deleleUseHash(@PathVariable("id") String id) {
+		
+		redisTemplate.opsForHash().delete("User", id);
+	    
+		return  "Delete user id: "+id +"completed";
+	}
+	
 }
